66cabd0b0878b00f3675a56561fe79582c1c694c
Prasanna Kumar Vanga <vangaprasannakumar@gmail.com>
2022-09-28 20:41:14 +0530

    loops

--- src/own/WhileLoop.java
+++ src/own/WhileLoop.java
@@ -1,24 +1,39 @@
 package own;
 import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class WhileLoop {
 	
 	public static void main(String[] args) {
+		@SuppressWarnings("resource")
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter PIN");
 		
 		short p = input.nextShort();
-
-		while(p!=1433) {
+		try {
 			if(p>=9999 || p<=999) {
-			System.out.println("Enter only 4 Digits PIN"+"\nTry again");
-			p = input.nextShort();
-			}
-			else if (p!=1433) {
-				System.out.println("The PIN incorrect. Try again");
+				System.out.println("Enter only 4 Digits PIN"+"\nTry again");
 				p = input.nextShort();
 			}
+			
+		}
+		catch(InputMismatchException e) {
+			System.out.println("Enter only 4 Digits PIN\"+\"\\nTry again");
+			
+		}
+		while(p!=1433)
+		{
+			
+				if(p>=9999 || p<=999) {
+					System.out.println("Enter only 4 Digits PIN"+"\nTry again");
+					p = input.nextShort();
+				}
+				else if (p!=1433) {
+					System.out.println("The PIN incorrect. Try again");
+					p = input.nextShort();
+				}
 		}
 		System.out.println("Correct PIN");
-	}		
+	}
 }
 
