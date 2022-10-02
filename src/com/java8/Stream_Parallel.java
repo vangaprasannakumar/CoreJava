df1d2eed85137b9bd7607e8cad6f1810071c8c5c
Prasanna Kumar Vanga <vangaprasannakumar@gmail.com>
2022-10-02 19:08:40 +0530

    Stream

---
+++ src/com/java8/Stream_Parallel.java
@@ -0,0 +1,22 @@
+package com.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stream_Parallel {
+
+	public static void main(String[] args) {
+		List<Character> obj = new ArrayList<Character>();
+		obj.add('P');
+		obj.add('S');
+		obj.add('K');
+		obj.add('U');
+		obj.add('H');
+		obj.add('R');
+		System.out.println("==== not in order====");
+		obj.parallelStream().forEach(System.out::println);
+		System.out.println("===== in order =====");
+		obj.parallelStream().forEachOrdered(x->System.out.println(x));
+	}
+
+}
---
+++ src/com/java8/Stream_Sequential.java
@@ -0,0 +1,17 @@
+package com.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stream_Sequential {
+	public static void main(String[] args) {
+	List<Character> obj = new ArrayList<Character>();
+	obj.add('P');
+	obj.add('S');
+	obj.add('K');
+	obj.add('U');
+	obj.add('H');
+	obj.add('R');
+	obj.stream().forEach(x->System.out.println(x));
+	}
+}
