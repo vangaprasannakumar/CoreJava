30195dd41809a54cf37c42cb2d4129059604b3e1
Prasanna Kumar Vanga <vangaprasannakumar@gmail.com>
2022-10-07 16:10:35 +0530

    Renaming  Package Names

---
+++ src/A1_practice/Variable_Demo.java
@@ -0,0 +1,29 @@
+package A1_practice;
+// // 2.2 Variable Demo
+class Student{
+	int id;
+	double marks;
+	static String college;
+}
+public class Variable_Demo {
+
+	public static void main(String[] args) {
+		Student rahul=new Student();
+		
+		rahul.id=124;
+		rahul.marks=968.4;
+		Student.college="Govt.Polytechnic Rudrampur";
+		
+		System.out.println("ID 	= "+rahul.id);
+		System.out.println("Marks 	= "+rahul.marks);
+		System.out.println("College Name = "+Student.college);
+		
+		System.out.println("--------------");
+		
+		Student kiran=new Student();
+		
+		System.out.println("ID 	= "+kiran.id);
+		System.out.println("Marks 	= "+kiran.marks);
+		System.out.println("College Name = "+Student.college);
+	}
+}
---
+++ src/A2_task/Variables_Demo.java
@@ -0,0 +1,32 @@
+package A2_task;
+
+class Student{
+	String name;
+	short id;
+	long number;
+	double marks;
+	char grade;
+	static String college;
+	static int clgcode;
+}
+public class Variables_Demo {
+	
+	public static void main(String[] args) {
+		Student lucky=new Student();
+		lucky.name="V. Prasanna Kumar";
+		lucky.id=124;
+		lucky.marks=968.4;
+		lucky.grade='A';
+		lucky.number=919963961371l;
+		Student.college="MIST";
+		Student.clgcode=9790;
+		
+		System.out.println("Name 		=  "+lucky.name);
+		System.out.println("ID 		=  "+lucky.id);
+		System.out.println("Number 		=  +"+lucky.number);
+		System.out.println("Marks 		=  "+lucky.marks);
+		System.out.println("Grade 		=  "+lucky.grade+"+");
+		System.out.println("College Name 	=  "+Student.college);
+		System.out.println("College code 	=  "+Student.clgcode);
+	}
+}
