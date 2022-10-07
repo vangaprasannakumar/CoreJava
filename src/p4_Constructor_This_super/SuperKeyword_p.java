b6d723d2dbb9834e33c4a577c1a298e1669aba37
Prasanna Kumar Vanga <vangaprasannakumar@gmail.com>
2022-10-07 17:46:38 +0530

    Adding Packages

---
+++ src/p4_Constructor_This_super/SuperKeyword_p.java
@@ -0,0 +1,19 @@
+package p4_Constructor_This_super;
+class Parent{
+	Parent(int age){
+		System.out.println("Parent age is "+age);
+	}
+}
+class Child extends Parent{
+	
+	Child(int age){
+		super(45);
+		System.out.println("Child age is "+age);
+	}
+}
+public class SuperKeyword_p {
+
+	public static void main(String[] args) {
+		Child obj = new Child(21);
+	}
+}
